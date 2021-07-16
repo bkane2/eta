bce7073538203d826dd6fd80b24c3024e7edb035
bkane2 <Ben.kane@me.com>
2021-07-16 15:19:09 -0400

    Added example to sophie-feedback rules-for-input

--- avatars/sophie-feedback/rules/eta-generation/reaction/choose-reaction-to-input.lisp
+++ avatars/sophie-feedback/rules/eta-generation/reaction/choose-reaction-to-input.lisp
@@ -1,6 +1,9 @@
 (READRULES '*reaction-to-input*
 ;
 '(
-  1 (An input was found 0)
-    2 (You gave an input \.) (0 :out)
+  ;; 1 (An input was found 0)
+  ;;   2 (You gave an input \.) (0 :out)
+
+  1 (Suggestion \: 0)
+    2 (You could have asked \: 3) (0 :out)
 ))
--- avatars/sophie-feedback/rules/user-interpretation/gist/rules-for-input.lisp
+++ avatars/sophie-feedback/rules/user-interpretation/gist/rules-for-input.lisp
@@ -6,8 +6,16 @@
   1 (bye)
     2 ((Goodbye \.)) (0 :gist)
 
-  1 (0 [SEP] 0)
-    2 ((An input was found \: 1 and 3 \.)) (0 :gist)
+  ;; 1 (0 [SEP] 0)
+  ;;   2 ((An input was found \: 1 and 3 \.)) (0 :gist)
+
+  ; Top-level nodes match SOPHIE's responses where we previously had opportunity tags
+  1 (WH_ does this mean for my future [SEP] 0)
+    ; Second-level nodes detect whether user asked an open-ended question or not
+    2 (0 [SEP] 0 WH_ worries you the most 0)
+      3 ((The doctor asked an open ended question \.)) (0 :gist)
+    ; If doctor did not ask an open-ended question, gist-clause stores suggestion
+    2 ((Suggestion \: what worries you the most ?)) (0 :gist)
   
   1 (0)
     2 ((NIL Gist \: nothing found for input \.)) (0 :gist)
