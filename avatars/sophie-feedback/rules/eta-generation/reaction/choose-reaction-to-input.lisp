9f9fcb7d4527e810eb7ea17929b34f0ab54eda3b
cgiugno <67064875+cgiugno@users.noreply.github.com>
2021-08-03 16:11:40 -0400

    Additional Rule for Reaction, SOPHIE Feedback

--- avatars/sophie-feedback/rules/eta-generation/reaction/choose-reaction-to-input.lisp
+++ avatars/sophie-feedback/rules/eta-generation/reaction/choose-reaction-to-input.lisp
@@ -1,9 +1,12 @@
 (READRULES '*reaction-to-input*
-;
 '(
   ;; 1 (An input was found 0)
   ;;   2 (You gave an input \.) (0 :out)
 
   1 (Suggestion \: 0)
     2 (You could have asked \: 3) (0 :out)
+
+  1 (The doctor asked an open ended question \.)
+    2 (Excellent\! You asked an open ended question \!) (0 :out)
+
 ))
