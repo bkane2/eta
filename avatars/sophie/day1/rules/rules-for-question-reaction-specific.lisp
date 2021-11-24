f54c24f492f1115348541d708e4da858e2c3b97a
cgiugno <67064875+cgiugno@users.noreply.github.com>
2021-11-24 11:53:01 -0500

    11-24 Troubleshooting P2

--- avatars/sophie/day1/rules/rules-for-question-reaction-specific.lisp
+++ avatars/sophie/day1/rules/rules-for-question-reaction-specific.lisp
@@ -2,6 +2,27 @@
 ; This tree can be used to define reactions to questions specific to module 1.
 ;
 '(
+  1 (0 comfort-care-word 0)
+    2 *redirect-to-prognosis* (0 :schema)
+
+  1 (0 treatment 1 options 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 treatment 1 goals 0)
+    2 *redirect-to-prognosis* (0 :schema)
+
+  1 (0 medicine 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 pain-med 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 pain-med-other 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 blood-pressure-med 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 med-narcotic 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 medication 0)
+    2 *redirect-to-prognosis* (0 :schema)
+
   1 (0 chemotherapy 0)
     2 *redirect-to-prognosis* (0 :schema)
 )) ; END *reaction-to-question-specific-session*
--- avatars/sophie/day1/rules/rules-for-statement-reaction-specific.lisp
+++ avatars/sophie/day1/rules/rules-for-statement-reaction-specific.lisp
@@ -2,6 +2,21 @@
 ; This tree can be used to define reactions to statements specific to module 1.
 ;
 '(
+  1 (0 comfort 1 care 0)
+    2 *redirect-to-prognosis* (0 :schema)
+
+  1 (0 medication 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 med-narcotic 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 take 1 different 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  
+  1 (0 treatment 1 option 0)
+    2 *redirect-to-prognosis* (0 :schema)
+  1 (0 cancer-goals 0)
+    2 *redirect-to-prognosis* (0 :schema)
+
   1 (0 chemotherapy 0)
     2 *redirect-to-prognosis* (0 :schema)
 )) ; END *reaction-to-statement-specific-session*
