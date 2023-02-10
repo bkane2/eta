8ce009f66fb673f4b6277aa034480896768edd4d
bkane2 <Ben.kane@me.com>
2023-02-10 01:10:30 -0500

    Added session subdirectories to make it easier to select/modify modules

---
+++ avatars/sophie-gpt/day2/schemas/eta-schema.lisp
@@ -0,0 +1,93 @@
+;; *ETA-SCHEMA*: development version 6
+;;
+;; TODO
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defparameter *eta-schema*
+
+'(event-schema :header (((set-of ^me ^you) have-eta-dialog.v) ** ?e)
+;`````````````````````````````````````````````````````````````````````
+; An Eta dialogue focused around a simple doctor-patient discussion, intended to test the integration
+; of GPT-3 with response generation.
+;
+; Explicit module
+;
+
+:types (
+  !t1 (^me person.n)
+  !t2 (^you person.n)
+)
+
+:rigid-conds (
+  ; Sophie is a woman
+  !r1 (^me ((pres be.v) (= (a.d woman.n))))
+  ; Sophie is 65 years old
+  !r2 (^me ((pres be.v) ((mod-a (65.a (plur year.n))) old.a)))
+)
+
+:static-conds (
+  ; Sophie has lung cancer
+  ?s1 (^me ((pres have.v) (k (lung.n cancer.n))))
+)
+
+:episodes (
+
+?e1 (^me say-to.v ^you '(Hi\, doctor\. I\'m meeting with you today to help get some questions answered about my condition\.))
+
+;; (prognosis)
+?e2 ((set-of ^me ^you) ask-about-prognosis.v)
+
+)
+
+)) ; END defparameter *eta-schema*
+
+
+
+;````````````````````````````````````````````````````````
+; Store schema variable name under header in *schemas*
+;
+(store-schema-name 'have-eta-dialog.v '*eta-schema*)
+
+
+
+;````````````````````````````````````````````````````````
+; Create empty hash tables for semantics,
+; gist-clauses, and topic-keys
+;
+(setf (get '*eta-schema* 'semantics) (make-hash-table))
+(setf (get '*eta-schema* 'gist-clauses) (make-hash-table))
+(setf (get '*eta-schema* 'topic-keys) (make-hash-table))
+
+
+
+;````````````````````````````````````````````````````````
+; EL Semantics - Not yet used
+;
+(mapcar #'(lambda (x)
+      (store-output-semantics (first x) (second x) '*eta-schema*))
+  '()
+) ; END mapcar #'store-output-semantics
+
+
+
+;````````````````````````````````````````````````````````
+; Gist clauses
+;
+(mapcar #'(lambda (x) 
+      (store-output-gist-clauses (first x) (second x) '*eta-schema*))
+  '()
+) ; END mapcar #'store-output-gist-clauses
+
+
+
+;````````````````````````````````````````````````````````
+; Topic keys
+;
+(mapcar #'(lambda (x) 
+      (store-topic-keys (first x) (second x) '*eta-schema*))
+  '(
+    (?e2 (Prognosis))
+  )
+) ; END mapcar #'store-topic-keys
